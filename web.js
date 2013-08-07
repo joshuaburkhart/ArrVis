afaa83d63970fc4a120617a64c502061cec449e8
Mosquito-Lab <mosquito-lab@mosquitolab-DX4870.(none)>
2013-08-07 13:16:26 -0700

    reading file

--- web.js
+++ web.js
@@ -5,26 +5,28 @@ var fs = require('fs');
 var app = express();
 
 app.use(express.logger());
+app.use(express.bodyParser());
 AWS.config.update({region: 'us-west-1'});
 
-app.get('/test.html', function(request, response) {
-    response.send("<html>\n<head>\n</head>\n<body>\n" + request.query.userInput + "\n</body>\n</html>");
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
 });
 
-var s3 = new AWS.S3({params: {Key: 'myKey'}});
-s3.createBucket(function() {
-    s3.putObject({Body: 'Hello!'}, function() {
-        console.log("Successfully uploaded data to " + process.env.S3_BUCKET_NAME + "/myKey");
-    });
+
+var s3 = new AWS.S3({params: {Bucket: 'arrvis', Key: 'myKey'}});
+s3.putObject({Body: 'Hello!'}, function() {
+    console.log("Successfully uploaded data to arrvis/myKey");
+});
+
+app.post('/test', function(request, response) {
+    var infile = request.files.datFile.path;
+    var contents = fs.readFileSync(infile);
+    response.send("<html>\n<head>\n</head>\n<body>\nFile Contents:<br> " + contents + "\n</body>\n</html>");
 });
 
 app.get('/', function(request, response) {
     var infile = "index.html";
     var contents = fs.readFileSync(infile);
     response.send(contents.toString());
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
 });
